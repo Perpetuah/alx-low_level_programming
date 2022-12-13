b0a55d6fd8e3b8374acba48730d2c887ff18b061
Perpetuah <pptuah@gmail.com>
2022-12-13 05:49:10 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - commutes the absolute value of an integer
+ * @t: The integer to be commuted
+ * Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
